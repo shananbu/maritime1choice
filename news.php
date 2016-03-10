c57e2c337de02c693861ec1fa6915690aea51d59
anbarasan.s <anbarasan.s@hcl.com>
2016-03-11 00:51:14 +0530

    update news

--- news.php
+++ news.php
@@ -1,27 +1,21 @@
+<?php include("admin/config.php"); ?>
 <div class="row">
-    <h1 class="h_1">Our latest news</h1>
+    <?php
+    $sql = "SELECT SUBSTRING(title, 1, 25) name, SUBSTRING(description, 1, 150) descr, DAY(createdDate) dy, SUBSTRING(MONTHNAME(createdDate), 1, 3) mname FROM News where status = 1 order by createdDate desc LIMIT 3";
+    $result = mysqli_query($iCon, $sql);
+    if(mysqli_num_rows($result) > 0) {
+    ?>
+        <h1 class="h_1">Our latest news</h1>
+    <?php }
+    while ($row = mysqli_fetch_assoc($result)) {
+    ?>
     <article class="col-sm-4">
-        <div class="new_date"><b>25</b> <span>Mar</span> </div>
+        <div class="new_date"><b><?php echo $row['dy']?></b> <span><?php echo $row['mname']?></span> </div>
         <div class="new_panel">
-            <h2 class="h_2">Sample News - 1</h2>
-            <span>Roman Reigns</span>
-            <p> Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium totam aperiam. </p>
+            <h2 class="h_2"><?php echo $row['name']?></h2>
+            <span></span>
+            <p> <?php echo $row['descr']?> </p>
             <a class="link_1" href="#"> <i class="fa fa-angle-double-right"></i> Read More</a> </div>
     </article>
-    <article class="col-sm-4">
-        <div class="new_date"><b>25</b> <span>Mar</span> </div>
-        <div class="new_panel">
-            <h2 class="h_2">Sample News - 1</h2>
-            <span>Roman Reigns</span>
-            <p> Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium totam aperiam. </p>
-            <a class="link_1" href="#"> <i class="fa fa-angle-double-right"></i> Read More</a> </div>
-    </article>
-    <article class="col-sm-4">
-        <div class="new_date"><b>25</b> <span>Mar</span> </div>
-        <div class="new_panel">
-            <h2 class="h_2">Sample News - 1</h2>
-            <span>Roman Reigns</span>
-            <p> Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium totam aperiam. </p>
-            <a class="link_1" href="#"> <i class="fa fa-angle-double-right"></i> Read More</a> </div>
-    </article>
+    <?php } ?>
 </div>
