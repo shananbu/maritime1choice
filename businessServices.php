7833e7f146dba69c964a4887e210674950a9e59f
anbarasan.s <anbarasan.s@hcl.com>
2016-03-12 12:06:14 +0530

    Some more changes

--- businessServices.php
+++ businessServices.php
@@ -75,75 +75,7 @@
                   <?php } ?>
               </div>
               <?php $catRowNum ++; } ?>
-            <div id="service_2" class="tab-pane fade">
-              <h1>Marine Software Solutions</h1>
-              <article>
-                <h1>Inventory Management:</h1>
-                <p>To streamline and to scale up savings companies started to focus on various aspects of operation one such is Procurement and its allied activities. To optimize spending in procuring, Companies must have a visibility in ROB. Maritime1stChoice offers inventory management as a key takeaway for its Clients. </p>
-                <small>Two Types of service offered such as:</small>
-                <h2>1. Baseline Support:</h2>
-                <p> Qualified and Experienced Inventory Team will board the vessel in up keeping of the Ship spares in line with the company Stock up keeping policy if available or done according to Industry best practices. This service is done primarily for beginners who opt Inventory Management for the first time or for a new build. Stock data collected is uploaded in onboard application and report will be submitted. </p>
-                <h2>2. Inventory Audit:</h2>
-                <p> Stock Team will board the vessel to ensure the Stock up keeping is in compliance with the company stock up keeping policy or industry best practices. Audit will be done in each store location provided basis a report will be submitted. Spare Data Audited will be cross verified with the application to avoid any deviation. </p>
-              </article>
-              <article>
-                <h1>DB Building:</h1>
-                <p>Our dedicated team has splendid experience in carving out successful outcomes for all your Data oriented proposals. We help shipping companies in transformation of Data into asset by value addition. <br>
-                  <br>
-                  Once Database was ignored gains importance in the recent past. An error free Database is the need of the hour. Keeping in this mind, Maritime1stChoice has designed a state of the art Business process with a sequential approach in building a database. Our Database building process is explained below in a nutshell: </p>
-                <ul>
-                  <li>Understand Client Requirement – starting of the Project and every week Discuss with Client for the Updates.</li>
-                  <li>Hard copy Equipment Technical manuals conversion to Electronic copies.</li>
-                  <li>Data coded convert into exclusively designed template with the aid of E copies.</li>
-                  <li>Data Conversion by Highly Qualified Personnel-under Domain expertise Guidance.</li>
-                  <li>Coded data will be Quality checked at various levels. Three gatekeepers need to approve the data template for error free at each stage.</li>
-                  <li>Template reaches the Quality Assurance team for final evaluation of the Data coded. If found to be free of errors, Data will uploaded Client Specified Application, Database will be delivered as injection files or excel sheets as per the clients requirement. Thus Quality is given at most important in all aspects.</li>
-                </ul>
-              </article>
-            </div>
-            <div id="service_3" class="tab-pane fade">
-              <h1>KPO services</h1>
-              <article>
-                <h1>DB Building:</h1>
-                <p>Our dedicated team has splendid experience in carving out successful outcomes for all your Data oriented proposals. We help shipping companies in transformation of Data into asset by value addition. <br>
-                  <br>
-                  Once Database was ignored gains importance in the recent past. An error free Database is the need of the hour. Keeping in this mind, Maritime1stChoice has designed a state of the art Business process with a sequential approach in building a database. Our Database building process is explained below in a nutshell: </p>
-                <ul>
-                  <li>Understand Client Requirement – starting of the Project and every week Discuss with Client for the Updates.</li>
-                  <li>Hard copy Equipment Technical manuals conversion to Electronic copies.</li>
-                  <li>Data coded convert into exclusively designed template with the aid of E copies.</li>
-                  <li>Data Conversion by Highly Qualified Personnel-under Domain expertise Guidance.</li>
-                  <li>Coded data will be Quality checked at various levels. Three gatekeepers need to approve the data template for error free at each stage.</li>
-                  <li>Template reaches the Quality Assurance team for final evaluation of the Data coded. If found to be free of errors, Data will uploaded Client Specified Application, Database will be delivered as injection files or excel sheets as per the clients requirement. Thus Quality is given at most important in all aspects.</li>
-                </ul>
-              </article>
-            </div>
-            <div id="service_4" class="tab-pane fade">
-              <h1>Miscellaneous</h1>
-              <article>
-                <h1>Inventory Management:</h1>
-                <p>To streamline and to scale up savings companies started to focus on various aspects of operation one such is Procurement and its allied activities. To optimize spending in procuring, Companies must have a visibility in ROB. Maritime1stChoice offers inventory management as a key takeaway for its Clients. </p>
-                <small>Two Types of service offered such as:</small>
-                <h2>1. Baseline Support:</h2>
-                <p> Qualified and Experienced Inventory Team will board the vessel in up keeping of the Ship spares in line with the company Stock up keeping policy if available or done according to Industry best practices. This service is done primarily for beginners who opt Inventory Management for the first time or for a new build. Stock data collected is uploaded in onboard application and report will be submitted. </p>
-                <h2>2. Inventory Audit:</h2>
-                <p> Stock Team will board the vessel to ensure the Stock up keeping is in compliance with the company stock up keeping policy or industry best practices. Audit will be done in each store location provided basis a report will be submitted. Spare Data Audited will be cross verified with the application to avoid any deviation. </p>
-              </article>
-              <article>
-                <h1>DB Building:</h1>
-                <p>Our dedicated team has splendid experience in carving out successful outcomes for all your Data oriented proposals. We help shipping companies in transformation of Data into asset by value addition. <br>
-                  <br>
-                  Once Database was ignored gains importance in the recent past. An error free Database is the need of the hour. Keeping in this mind, Maritime1stChoice has designed a state of the art Business process with a sequential approach in building a database. Our Database building process is explained below in a nutshell: </p>
-                <ul>
-                  <li>Understand Client Requirement – starting of the Project and every week Discuss with Client for the Updates.</li>
-                  <li>Hard copy Equipment Technical manuals conversion to Electronic copies.</li>
-                  <li>Data coded convert into exclusively designed template with the aid of E copies.</li>
-                  <li>Data Conversion by Highly Qualified Personnel-under Domain expertise Guidance.</li>
-                  <li>Coded data will be Quality checked at various levels. Three gatekeepers need to approve the data template for error free at each stage.</li>
-                  <li>Template reaches the Quality Assurance team for final evaluation of the Data coded. If found to be free of errors, Data will uploaded Client Specified Application, Database will be delivered as injection files or excel sheets as per the clients requirement. Thus Quality is given at most important in all aspects.</li>
-                </ul>
-              </article>
-            </div>
+
           </div>
         </section>
       </div>
