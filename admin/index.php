3a2ecbf3a6bb19297254a9a1f84cd53b40225f6f
anbarasan.s <anbarasan.s@hcl.com>
2016-03-13 22:13:08 +0530

    Some more changes

--- admin/index.php
+++ admin/index.php
@@ -25,48 +25,47 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 }
 ?>
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+
+<!DOCTYPE html>
+<html lang="en">
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1"/>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Login Page</title>
+    <!-- Bootstrap -->
+    <link href="css/admin.css" rel="stylesheet">
+    <!--[if lt IE 9]>
+    <script src="html5shiv.min.js"></script>
+    <script src="respond.min.js"></script>
+    <![endif]-->
 
-    <style type="text/css">
-        body {
-            font-family: Arial, Helvetica, sans-serif;
-            font-size: 14px;
-
-        }
-
-        label {
-            font-weight: bold;
-
-            width: 100px;
-            font-size: 14px;
-
-        }
-
-        .box {
-            border: #666666 solid 1px;
-
-        }
-    </style>
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.js"></script>
 </head>
-<body bgcolor="#FFFFFF">
-<div align="center">
-    <div style="width:300px; border: solid 1px #333333; " align="left">
-        <div style="background-color:#333333; color:#FFFFFF; padding:3px;"><b>Login</b></div>
-        <div style="margin:30px">
-            <form action="" method="post">
+<body>
+
+<header class="navbar-fixed-top top_header ful_row">
+    <div class="logo"> <img src="../images/logo.png"></div>
+    <h1>Maritime1stChoice.com's Admin </h1>
+</header>
+<section id="wrapper">
+    <section class="main_info">
+        <div class="login_box">
+            <form class = "form_li" action="" method="post">
                 <label>UserName :</label><input type="text" name="username" class="box"/><br/><br/>
                 <label>Password :</label><input type="password" name="password" class="box"/><br/><br/>
                 <input type="submit" value=" Submit "/><br/>
             </form>
-            <div style="font-size:11px; color:#cc0000; margin-top:10px"><?php echo $error; ?></div>
         </div>
-    </div>
-</div>
-
+    </section>
+</section>
+<script>
+    $("#menu-toggle").click(function (e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+    });
+</script>
 </body>
 </html>
+
